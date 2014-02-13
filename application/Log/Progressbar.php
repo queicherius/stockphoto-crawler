85c55cbe5513f56828bc0c0d1d90d9f399b15f35
queicherius <queicherius@gmx.de>
2014-02-13 14:28:25 +0100

    Progressbar for tasks that take some time

---
+++ application/Log/Progressbar.php
@@ -0,0 +1,42 @@
+<?php namespace Log;
+
+use Log;
+
+class Progressbar
+{
+
+    private static $color = '32';
+    private static $blocks = 20;
+
+    public function setProgress($percent)
+    {
+
+        $blocks = $this->calculateBlocks($percent);
+
+        $string = $this->renderBar($blocks) . $this->renderText($percent);
+
+        $end_of_line = ($percent === 100) ? "\n" : "\r";
+
+        Log::info($string, $end_of_line);
+
+    }
+
+    private function calculateBlocks($percent)
+    {
+        return floor($percent * (self::$blocks / 100));
+    }
+
+    private function renderText($percent)
+    {
+        return ' ' . floor($percent) . '%';
+    }
+
+    private function renderBar($blocks)
+    {
+        $done = Log::colorString(self::$color, str_repeat('#', $blocks));
+        $not_done = str_repeat(' ', self::$blocks - $blocks);
+
+        return 'Progress: [' . $done . $not_done . ']';
+    }
+
+}
