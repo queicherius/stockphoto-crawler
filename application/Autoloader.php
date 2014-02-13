c1b01f726b21e3e70b6f1810ca836da18a622413
queicherius <queicherius@gmx.de>
2014-02-13 14:28:51 +0100

    Added autoloader for classes

---
+++ application/Autoloader.php
@@ -0,0 +1,14 @@
+<?php
+
+spl_autoload_register(function ($class){
+
+    $path =  __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+
+    if (file_exists($path)) {
+        include ($path);
+        return true;
+    }
+
+    return false;
+
+});
