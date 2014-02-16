c7dafc56ddba4e88ca552299a4f31d4c1b8651c2
queicherius <queicherius@gmx.de>
2014-02-16 16:40:12 +0100

    Rewritten HTTP class with curl, fixes http version not supported errors (#1)

--- application/Http/Http.php
+++ application/Http/Http.php
@@ -3,64 +3,114 @@
 class Http
 {
 
+    private $curl_handle;
+
     public function fetch($url, $post_data = false)
     {
 
+        $this->initializeRequest($url);
+
         if ($post_data) {
-            $content = $this->post($url, $post_data);
-        } else {
-            $content = $this->get($url);
+            $this->setPostData($post_data);
         }
 
-        if (!$content) {
-            throw new HttpException("{$this->getLastError()}");
+        $content = $this->executeRequest();
+
+        if ($this->hasError()) {
+            throw new HttpException("{$this->getErrorMessage()}");
         }
+
+        $this->closeRequest();
 
         return $content;
 
     }
 
-    public function get($url)
-    {
-        return @file_get_contents($url);
-    }
-
-    public function post($url, $post_data)
+    private function initializeRequest($url)
     {
 
-        $stream_context = $this->streamContext($post_data);
-        return @file_get_contents($url, false, $stream_context);
+        $this->curl_handle = curl_init();
 
-    }
-
-    private function streamContext($post)
-    {
-
-        $post = http_build_query($post);
-        $options = [
-            'http' => [
-                'method'  => 'POST',
-                'header'  => 'Content-type: application/x-www-form-urlencoded',
-                'content' => $post
+        curl_setopt_array(
+            $this->curl_handle,
+            [
+                CURLOPT_URL            => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_SSL_VERIFYPEER => false
             ]
-        ];
-
-        return stream_context_create($options);
+        );
 
     }
 
-    private function getLastError()
+    private function setPostData($data)
     {
 
-        $error_message = error_get_last()['message'];
+        curl_setopt_array(
+            $this->curl_handle,
+            [
+                CURLOPT_POST       => 1,
+                CURLOPT_POSTFIELDS => http_build_query($data)
+            ]
+        );
 
-        // Remove url
-        $error_message = preg_replace('#file_get_contents\([^\)]*\): #', '', $error_message);
+    }
 
-        // Remove everything up to the HTTP error
-        $error_message = preg_replace('#^.*([0-9]{3})#', '$1', $error_message);
+    private function executeRequest()
+    {
+        return curl_exec($this->curl_handle);
+    }
 
-        return $error_message;
+    private function closeRequest()
+    {
+        curl_close($this->curl_handle);
+    }
+
+    private function hasError()
+    {
+
+        // Curl error
+        if (curl_error($this->curl_handle)) {
+            return true;
+        }
+
+        // Http error
+        if (curl_getinfo($this->curl_handle, CURLINFO_HTTP_CODE) !== 200) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    private function getErrorMessage()
+    {
+
+        // Curl error
+        if (curl_error($this->curl_handle)) {
+            return curl_error($this->curl_handle);
+        }
+
+        // Http error
+        if (curl_getinfo($this->curl_handle, CURLINFO_HTTP_CODE) !== 200) {
+            return $this->httpErrorMessage(curl_getinfo($this->curl_handle, CURLINFO_HTTP_CODE));
+        }
+
+    }
+
+    private function httpErrorMessage($error_code)
+    {
+
+        switch ($error_code) {
+            case 404:
+                return '404 not found';
+            case 503:
+                return '503 service unavailable';
+            case 509:
+                return '509 bandwidth limit exceeded';
+            default:
+                return 'HTTP ' . $error_code;
+        }
 
     }
 
